a247ccd9e92107687af30398978e2ef324905686
Kirill Savin <kirsavin33@gmail.com>
2024-11-12 00:15:36 +0300

    Refactored models.py and other improvements

--- bot_commands/help.py
+++ bot_commands/help.py
@@ -7,7 +7,7 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Function logic"""
     if update.effective_chat is not None:
         message = (
-            ''
+            'Here is nobody except you'
         )
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
--- models.py
+++ models.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base, Mapped, mapped_column
 
 Base = declarative_base()  # базовый класс для создания моделей
 
 
 class Item(Base):
     __tablename__ = 'items'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
-    price = Column(Float, nullable=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    price: Mapped[float] = mapped_column(Float, nullable=True)
 
     def __repr__(self):
         return str(self.name)
@@ -16,8 +16,8 @@ class Item(Base):
 
 class CartItem(Base):
     __tablename__ = 'cart_items'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
-    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
-    quantity = Column(Integer, default=1)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    item_id: Mapped[int] = mapped_column(Integer, ForeignKey('items.id'), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, default=1)
     item = relationship('Item')
